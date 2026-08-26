2495533abf0e1d46d82a3733a32a47d92c383b56
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-08-27 03:32:36 +0500

    update product screen

--- lib/features/product/product_screen.dart
+++ lib/features/product/product_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/extension_instance.dart';
 import 'package:retailcore_pos/core/widgets/custom_app_bar.dart';
@@ -17,30 +16,55 @@ class ProductScreen extends StatelessWidget {
     return Scaffold(
       appBar: CustomAppBar(name: "VIEW PRODUCTS"),
 
-      body: Obx(()=>ListView.builder(itemCount: pC.products.length,itemBuilder:(context,index){
-        return Column(
-          children: [
-            Row(
-              children: [
-                Text("Id: ${pC.products[index]["ID"]}"),
-                Text("Name: ${pC.products[index]["NAME"]}"),
-                Text("Quantity: ${pC.products[index]["QUANTITY"]}"),
-                Text("Price: ${pC.products[index]["PRICE"]}"),
-                Text("Barcode: ${pC.products[index]["BARCODE"]}"),
-              ],
-            ),
-            Row(
-              children: [
-                ElevatedButton(onPressed: ()async{
-                  await pC.deleteProduct(pC.products[index]["ID"]);
-                  await pC.showProducts();
-                }, child: Text("Delete Product")),
+      body: Obx(()=> pC.products.isNotEmpty ? ListView.builder(itemCount: pC.products.length,itemBuilder:(context,index){
+        return Container(
+          padding: EdgeInsets.all(8),
+          margin: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+              border: Border.all(color: Colors.blueAccent,width: 1)
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(8),
+                  margin: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(color: Colors.greenAccent,width: 1)
+                  ),
+                  child: Column(
+                    children: [
+                      Text("Id: ${pC.products[index]["ID"]}",style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold,color: Colors.black,),),
+                      Text("Name: ${pC.products[index]["NAME"]}",style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold,color: Colors.black,)),
+                      Text("Quantity: ${pC.products[index]["QUANTITY"]}",style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold,color: Colors.black,)),
+                      Text("Price: ${pC.products[index]["PRICE"]}",style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold,color: Colors.black,)),
+                      Text("Barcode: ${pC.products[index]["BARCODE"]}",style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold,color: Colors.black,)),
+                    ],
+                  ),
+                ),
+              ),
 
-              ],
-            )
-          ],
+                  ElevatedButton(onPressed: ()async{
+                    await pC.deleteProduct(pC.products[index]["ID"]);
+                    await pC.showProducts();
+                  },
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.blueAccent,padding: EdgeInsets.all(4))
+        ,child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("Delete Product",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,)),
+          ),
+        )),
+            ],
+
+          ),
         );
       })
-      ));
+      : Text("No Product Added",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black,))));
   }
 }
