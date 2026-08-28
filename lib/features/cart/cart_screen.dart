d85ab145373ff4d3376095bf0a6e68e5804377f5
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-08-28 06:02:42 +0500

    implemented cart functionality

---
+++ lib/features/cart/cart_screen.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/src/extension_instance.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+
+import '../../core/widgets/custom_app_bar.dart';
+import '../new_sales/new_sales_controller.dart';
+
+class CartScreen extends StatelessWidget {
+  CartScreen({super.key});
+
+  NewSalesController pC=Get.find<NewSalesController>();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: CustomAppBar(name: "View Cart"),
+
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.fromLTRB(33,0,0,0),
+          child: SizedBox(
+            width: double.infinity,
+            child: FloatingActionButton.extended(onPressed: (){
+              //Navigator.push(context, MaterialPageRoute(builder: (context)=>CartScreen(productsQuantity:pC.productsQuantity)));
+            },
+              backgroundColor: Colors.greenAccent,
+              label: Row(
+                children: [
+                  Text("View Cart",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,)),
+                  SizedBox(width: 8,),
+                  Icon(Icons.trolley,color: Colors.white,)
+                ],
+              ),),
+          ),
+        ),
+
+        body: Obx(()=> pC.products.isNotEmpty ? ListView.builder(itemCount: pC.productsQuantity.length,itemBuilder:(context,index){
+          return Container(
+            padding: EdgeInsets.all(8),
+            margin: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(color: Colors.blueAccent,width: 1)
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    margin: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                        border: Border.all(color: Colors.greenAccent,width: 1)
+                    ),
+                    child: Column(
+                      children: [
+                        Text("Id: ${pC.products[pC.productsQuantity[index]["INDEX"]]["ID"]}",style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold,color: Colors.black,),),
+                        Text("Name: ${pC.products[pC.productsQuantity[index]["INDEX"]]["NAME"]}",style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold,color: Colors.black,)),
+                        Text("Quantity: ${pC.products[pC.productsQuantity[index]["INDEX"]]["QUANTITY"]}",style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold,color: Colors.black,)),
+                        Text("Price: ${pC.products[pC.productsQuantity[index]["INDEX"]]["PRICE"]}",style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold,color: Colors.black,)),
+                        Text("Barcode: ${pC.products[pC.productsQuantity[index]["INDEX"]]["BARCODE"]}",style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold,color: Colors.black,)),
+                        Obx(()=>Text("Cart: ${pC.screenQuantityDisplay[pC.productsQuantity[index]["INDEX"]]}",style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold,color: Colors.black,))),
+
+
+                      ],
+                    ),
+                  ),
+                ),
+
+                Column(
+                  children: [
+                    ElevatedButton(onPressed: (){
+
+                        if(pC.productsQuantity[index]["QUANTITY"]>= pC.products[pC.productsQuantity[index]["INDEX"]]["QUANTITY"]){
+                          return;
+                        }
+                        pC.productsQuantity[index]["QUANTITY"]++;
+                        pC.screenQuantityDisplay[pC.productsQuantity[index]["INDEX"]]++;
+
+                        pC.productsQuantity.refresh();
+                        pC.screenQuantityDisplay.refresh();
+
+
+                    },
+                        style: ElevatedButton.styleFrom(backgroundColor: Colors.blueAccent,padding: EdgeInsets.all(4))
+                        ,child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("+",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,)),
+                          ),
+                        )),
+
+                    ElevatedButton(onPressed: (){
+
+                        if(pC.productsQuantity[index]["QUANTITY"]==1){
+                          pC.productsQuantity.removeAt(index);
+                          pC.screenQuantityDisplay[pC.productsQuantity[index]["INDEX"]]--;
+                          pC.productsQuantity.refresh();
+                          pC.screenQuantityDisplay.refresh();
+                        }
+                        else{
+                          pC.productsQuantity[index]["QUANTITY"]--;
+                          pC.screenQuantityDisplay[pC.productsQuantity[index]["INDEX"]]--;
+                          pC.productsQuantity.refresh();
+                          pC.screenQuantityDisplay.refresh();
+                        }
+                    },
+                        style: ElevatedButton.styleFrom(backgroundColor: Colors.blueAccent,padding: EdgeInsets.all(4))
+                        ,child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("-",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,)),
+                          ),
+                        )),
+                  ],
+                ),
+              ],
+
+            ),
+          );
+        })
+            : Text("No Product Added",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black,))));;
+  }
+}
